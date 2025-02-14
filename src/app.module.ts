64b28f25387d62d1d8a3a3f053f980fc2ba38f97
XaydarovMarufjon <marufjankhaydarov@gmail.com>
2025-02-14 12:16:15 +0500

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import { AppService } from './app.service';
 import { DocumentModule } from './document/document.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [DocumentModule,
     MulterModule.register({
       dest: './uploads',
     }),
-    MongooseModule.forRoot('mongodb+srv://marufjankhaydarov:l8OfkMCsirRA897X@listdoc.h03kn.mongodb.net/?retryWrites=true&w=majority&appName=listdoc')
+    MongooseModule.forRoot('mongodb+srv://marufjankhaydarov:l8OfkMCsirRA897X@listdoc.h03kn.mongodb.net/?retryWrites=true&w=majority&appName=listdoc'),
+    ConfigModule.forRoot({
+      isGlobal: true, // .env ni global qilib qo‘yish
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
